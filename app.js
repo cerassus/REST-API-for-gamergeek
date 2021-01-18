d22719e9a8b1fef58abbc7e5875c252d2d5389b6
Michał Wiśniewski <cerassus@localhost.localdomain>
2021-01-18 09:00:10 +0100

    app

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const cron = require('node-cron');
 const app = express();
 const routes = require("./routes");
 const getGameDatabase = require("./updateGameDatabase");
-const emailjs = require('emailjs-com');
 const corsOptions = {
   optionsSuccessStatus: 200,
   methods: "GET, POST, PUT",
 };
-const fetch = require('node-fetch');
 require("dotenv/config");
-
-// e-mail send config
-
-
 
 // use Middleware
 
@@ -31,26 +25,15 @@ mongoose.connect(process.env.DB_CONNECTION, {
   useUnifiedTopology: true,
 });
 
-// emailjs.send("service_shv15zg", "template_t2arx88", {
-//   from_name: "Gamergeek REST API",
-//   message: "Database connected",
-//   reply_to: "cerassus@gmail.com",
-// },"user_tAqKspPCvgbXOIGmjkApr");screenLeft
-
-fetch("https://api.emailjs.com/api/v1.0/email/send", {
-  headers: {
-    "Content-Type": "application/json",
-  },
-  method: "POST",
-  data: JSON.stringify({
-    "user_id": "user_tAqKspPCvgbXOIGmjkApr",
-    "service_id": "service_shv15zg",
-    "template_id": "template_t2arx88",
-  }),
-})
-
 // update game database twice a month at 04:00 AM
 
-app.listen(3001, function () {
+cron.schedule('0 4 1,20 * *', () => {
+  console.log('starting CRON task...')
+  getGameDatabase()
+})
+
+// Listen on PORT 3000
+
+app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
